185cbf3d2e788b1f227f03d388027c7a8e471008
Sven van den Tweel <sven.vd.tweel@gmail.com>
2020-10-17 17:54:32 +0200

    initial commit for library funtionality

--- app/src/main/java/nl/invissvenska/pulsatingripple/FirstFragment.java
+++ app/src/main/java/nl/invissvenska/pulsatingripple/FirstFragment.java
@@ -1,13 +1,21 @@
 package nl.invissvenska.pulsatingripple;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import java.util.ArrayList;
 
 public class FirstFragment extends Fragment {
 
@@ -23,12 +31,46 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+//        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(FirstFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+//            }
+//        });
+       final PulsatingLayout pl = view.findViewById(R.id.ripple);
+        pl.startRippleAnimation();
+
+//        final Handler handler=new Handler();
+
+
+//        ImageView button=view.findViewById(R.id.centerImage);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                pl.startRippleAnimation();
+//                handler.postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+////                        foundDevice();
+//                    }
+//                },3000);
+//            }
+//        });
+
     }
+
+//    private void foundDevice(){
+//        AnimatorSet animatorSet = new AnimatorSet();
+//        animatorSet.setDuration(400);
+//        animatorSet.setInterpolator(new AccelerateDecelerateInterpolator());
+//        ArrayList<Animator> animatorList=new ArrayList<Animator>();
+//        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(foundDevice, "ScaleX", 0f, 1.2f, 1f);
+//        animatorList.add(scaleXAnimator);
+//        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(foundDevice, "ScaleY", 0f, 1.2f, 1f);
+//        animatorList.add(scaleYAnimator);
+//        animatorSet.playTogether(animatorList);
+//        foundDevice.setVisibility(View.VISIBLE);
+//        animatorSet.start();
+//    }
 }
